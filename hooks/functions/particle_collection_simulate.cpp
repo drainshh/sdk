58d593ce244fff80ac1c9ef9f3d149c41ade3d57
drainshh <150180787+drainshh@users.noreply.github.com>
2026-04-26 21:26:56 +0200

    Refactor particle_collection_simulate with new structure
    Removed old hook function and added particle_collection_t structure.

--- hooks/functions/particle_collection_simulate.cpp
+++ hooks/functions/particle_collection_simulate.cpp
@@ -1,17 +1,14 @@
 #include "../hooks.hpp"
 #include "../functions/particle_collection_simulate.h"
 
-namespace sdk::hooks::particle_collection_simulate {
+struct particle_system_definition_t {
+    unsigned char pad0[308];
+    char* name;
+};
 
-    void __fastcall hook(void* ecx, void* edx)
-    {
-        static bool once = false;
-
-        if (!once) {
-            debug::log("PARTICLE SIM FIRST HIT ecx=%p", ecx);
-            once = true;
-        }
-
-        ofunc(ecx);
-    }
-}
+struct particle_collection_t {
+    unsigned char pad0[48];
+    int active_particles;
+    unsigned char pad1[12];
+    void* def_ref; // may differ in your SDK, so don't use if crashes
+};
