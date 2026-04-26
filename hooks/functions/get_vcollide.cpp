abe18767f7128d5e9c56c5b5f8bcd6547ade3d6c
drainshh <150180787+drainshh@users.noreply.github.com>
2026-04-27 00:45:29 +0200

    Enhance get_vcollide with debug logging
    Add logging for get_vcollide when model_index is -1.

--- hooks/functions/get_vcollide.cpp
+++ hooks/functions/get_vcollide.cpp
@@ -3,8 +3,17 @@
 
 void* __fastcall sdk::hooks::get_vcollide::get_vcollide(void* ecx, void* edx, int model_index)
 {
-    if (model_index == -1)
-        return features::weather::getv_collideble();
+    if (model_index == -1) {
+        static int printed = 0;
+        void* collide = features::weather::getv_collideble();
+
+        if (printed < 20) {
+            debug::log("GETVCOLLIDE -1 HIT collide=%p", collide);
+            ++printed;
+        }
+
+        return collide;
+    }
 
     return ofunc(ecx, edx, model_index);
 }
