77a9de6afc5e7c37a38f294db69edb4c0d1c29f3
drainshh <150180787+drainshh@users.noreply.github.com>
2026-04-26 21:32:05 +0200

    Implement hook for particle_collection_simulate
    Added a hook function for particle collection simulation with logging.

--- hooks/functions/particle_collection_simulate.cpp
+++ hooks/functions/particle_collection_simulate.cpp
@@ -1,14 +1,18 @@
 #include "../hooks.hpp"
 #include "../functions/particle_collection_simulate.h"
 
-struct particle_system_definition_t {
-    unsigned char pad0[308];
-    char* name;
-};
+namespace sdk::hooks::particle_collection_simulate {
 
-struct particle_collection_t {
-    unsigned char pad0[48];
-    int active_particles;
-    unsigned char pad1[12];
-    void* def_ref; // may differ in your SDK, so don't use if crashes
-};
+    void __fastcall hook(void* ecx, void* edx)
+    {
+        static bool once = false;
+
+        ofunc(ecx);
+
+        if (!once) {
+            debug::log("PARTICLE SIM FIRST HIT ecx=%p", ecx);
+            once = true;
+        }
+    }
+
+}
