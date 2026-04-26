80fa1aa079635e6861a3da25705327677253e661
drainshh <150180787+drainshh@users.noreply.github.com>
2026-04-26 16:58:48 +0200

    Refactor get_vcollide to simplify logic
    Removed logging and call count for get_vcollide function.

--- hooks/functions/get_vcollide.cpp
+++ hooks/functions/get_vcollide.cpp
@@ -3,27 +3,8 @@
 
 void* __fastcall sdk::hooks::get_vcollide::get_vcollide(void* ecx, void* edx, int model_index)
 {
-    static int call_count = 0;
-
-    if (call_count < 20) {
-        debug::log("GETVCOLLIDE CALL #%d model_index=%d override=%d loaded=%d",
-            call_count,
-            model_index,
-            features::weather::should_override_vcollide(model_index) ? 1 : 0,
-            features::weather::getv_collideble() ? 1 : 0
-        );
-        ++call_count;
-    }
-
-    if (features::weather::should_override_vcollide(model_index)) {
-        void* collide = features::weather::getv_collideble();
-
-        debug::log("GETVCOLLIDE: overriding model_index=%d collideable=%p",
-            model_index, collide);
-
-        if (collide)
-            return collide;
-    }
+    if (model_index == -1)
+        return features::weather::getv_collideble();
 
     return ofunc(ecx, edx, model_index);
 }
