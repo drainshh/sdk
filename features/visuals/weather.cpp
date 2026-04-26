24fb93aaa9bcdb2143625c0ceefd9dee9133bae3
drainshh <150180787+drainshh@users.noreply.github.com>
2026-04-26 23:24:06 +0200

    Enable rain and precipitation settings in console

--- features/visuals/weather.cpp
+++ features/visuals/weather.cpp
@@ -221,4 +221,21 @@ void features::weather::update_weather() {
         rain_entity->get_model_index(),
         rain_entity->get_index(),
         precipitation_collision_loaded ? 1 : 0);
+
+    if (interfaces::console) {
+        if (auto cvar = interfaces::console->get_convar("r_DrawRain"))
+            cvar->set_value(1);
+        if (auto cvar = interfaces::console->get_convar("r_DrawPrecipitation"))
+            cvar->set_value(1);
+        if (auto cvar = interfaces::console->get_convar("r_RainSimulate"))
+            cvar->set_value(1);
+        if (auto cvar = interfaces::console->get_convar("r_RainParticleDensity"))
+            cvar->set_value(1.0f);
+        if (auto cvar = interfaces::console->get_convar("r_RainRadius"))
+            cvar->set_value(4096.0f);
+        if (auto cvar = interfaces::console->get_convar("r_RainHack"))
+            cvar->set_value(1);
+        if (auto cvar = interfaces::console->get_convar("r_rainalpha"))
+            cvar->set_value(1.0f);
+    }
 }
